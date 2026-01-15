af7e38ae491bc3ccbb1b4b844295d712a5ebead5
Asyabi <asyabicp@gmail.com>
2026-01-15 11:27:34 +0530

    Modified append.js

---
+++ linkedlist/append.js
@@ -0,0 +1,63 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    isEmpty() {
+        return this.size === 0;
+    }
+
+    getSize() {
+        return this.size;
+    }
+
+    append(value) {
+        const node = new Node(value);
+
+        if (this.isEmpty()) {
+            this.head = node;
+        } else {
+            let curr = this.head;
+            while (curr.next !== null) {
+                curr = curr.next;
+            }
+            curr.next = node;
+        }
+        this.size++;
+    }
+
+    print() {
+        if (this.isEmpty()) {
+            console.log("List is empty");
+            return;
+        }
+
+        let curr = this.head;
+        let result = "";
+
+        while (curr !== null) {
+            result += curr.value + " -> ";
+            curr = curr.next;
+        }
+
+        console.log(result + "null");
+    }
+}
+
+const list = new LinkedList();
+console.log(list.isEmpty()); 
+console.log(list.getSize()); 
+
+list.append(10);
+list.append(20);
+list.append(30);
+
+list.print(); 
